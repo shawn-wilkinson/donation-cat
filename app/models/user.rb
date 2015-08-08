caade739172461fcc655c83b56a76573a08407d5
LKompare <lkompare87@gmail.com>
2015-08-08 13:24:45 -0500

    fix pass digest to pass hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,11 @@ class User < ActiveRecord::Base
 
 
   def password=(password)
-    self.password_digest = BCrypt::Password.create(password)
+    self.password_hash = BCrypt::Password.create(password)
   end
 
   def is_password?(password)
-    BCrypt::Password.new(self.password_digest) == password
+    BCrypt::Password.new(self.password_hash) == password
   end
 
 
