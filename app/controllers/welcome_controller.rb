24b31a7d0e1dfc10401ec4f01b533ce6c576d9dd
LKompare <lkompare87@gmail.com>
2015-08-09 15:01:42 -0500

    fix home page and index layout, user login and sign up are working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
+  	if session[:user_id]
+    	@name = User.find(session[:user_id])
+    end
     @categories = Category.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-
+  
   get 'charities/id/:star' => 'charities#star', as: :star
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
