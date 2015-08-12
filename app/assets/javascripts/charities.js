65732e475979cb980a1f9284398682330eda80eb
peterhinners <peterhinners@gmail.com>
2015-08-12 15:59:33 -0500

    ajaxed add new amazon wishlist

--- app/assets/javascripts/charities.js
+++ app/assets/javascripts/charities.js
@@ -1,9 +1,40 @@
 $(document).ready(function() {
     
-	console.log("hey");
 
-	$(document).on("click",".wishlist-class",function(){
-	console.log("yomama");
+
+	$(document).on("click",".wishlist-class",function(event){
+		event.preventDefault();
+	
+
+	// var hardCodedUrl = "http://localhost:3000/charities/pete's-cats-8/wishlists/new";
+
+		var that = $(this);
+		var request = $.ajax({
+    url: $(this).attr('ajax_path'), 
+    // url: hardCodedUrl,
+    method: 'GET'
+    
+  });
+
+		request.done(function(response){
+			console.log("shazaam");
+			console.log(response);
+			that.hide();
+			$("#ajaxify").prepend(response);
+		});
+
+
+
+	// 	var route = window.location.pathname.concat('/map');
+	//   var request = $.ajax({url: route, method: 'GET', dataType: "json"});
+	//   request.done(function(response){
+	//       var latlong = { lat: response["latitude"], lng: response["longitude"] };
+	//       var name = response["name"];
+	//       generateMap(latlong, name)
+	// });
+
+	// wishlists/new
+	// <%= link_to 'Edit', '#', :id => "edit_comment_link", :ajax_path => edit_post_comment_path(@post, @comment) %>
 
 //   var id = $(this).data("id");
 //   var href = $(this).data("href");
