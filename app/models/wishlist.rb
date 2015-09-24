f0ef5659dc6ade451f8cc5f5fe748a3bfb143293
peterhinners <peterhinners@gmail.com>
2015-09-24 15:56:17 -0500

    improvements

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -39,9 +39,9 @@ class Wishlist < ActiveRecord::Base
 			if link
 				destination = "http://www.amazon.com/gp/registry/wishlist/ref=cm_wl_act_print_v?ie=UTF8&disableNav=1&filter=all&id=" + initial_regex(link) + "&items-per-page=200&layout=standard-print&sort=date-added&visitor-view=1"
 
-					doc = Nokogiri::HTML(open(destination))
-					track_row = doc.css("tr")
-					modified_track_rows = track_row[3..-1]
+					doc = Nokogiri::HTML(open(destination)) if destination
+					track_row = doc.css("tr") if doc
+					modified_track_rows = track_row[3..-1] if track_row
 					if modified_track_rows
 						modified_track_rows.each do |track_row|
 						name = track_row.css(".g-title").first.text
@@ -55,7 +55,7 @@ class Wishlist < ActiveRecord::Base
 	  					name = name.encode("UTF-16be", :invalid=>:replace, :replace=>"?").encode('UTF-8')
 						end
 
-						item = Item.new(name: name, picture_url: picture_url, current_price: current_price, priority: priority, qty_requested: qty_requested, qty_received: qty_received)
+						item = Item.new(name: name, picture_url: picture_url, current_price: current_price, priority: priority, qty_requested: qty_requested, qty_received: qty_received) if name && picture_url && current_price && priority && qty_received && qty_requested
 
 						self.items << item
 						end
